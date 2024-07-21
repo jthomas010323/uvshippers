3b33d4c18f9d2482725baf3a0a398bbd5b05754e
Justin Thomas <jthomas010323@gmail.com>
2024-07-20 22:16:40 -0400

    under construction complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 import uvshippersLogo from "./assets/uvshippers-logo.png";
+import upsLogo from "./assets/ups-logo-png-transparent.png";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
-        <a href="#" target="_blank">
+        <a target="_blank">
           <img
             src={uvshippersLogo}
             className="h-60 mx-auto"
@@ -16,11 +15,20 @@ function App() {
           />
         </a>
       </div>
-      <h1>UVSHIPPERS</h1>
-      <div className="card">
-        <p>Site is currently UNDER CONSTRUCTION</p>
+      <h1>UNIVERSAL SHIPPERS NOW OPEN</h1>
+      <div>
+        <img src={upsLogo} className="logo" />
+        <h2> Authorized UPS Shipping Center</h2>
       </div>
-      <p className="read-the-docs">Come back later!</p>
+      <div className="card">
+        <h3>
+          Come visit us at{" "}
+          <span style={{ textDecoration: "underline" }}>
+            2246 Victory Blvd, Staten Island, NY 10314
+          </span>
+        </h3>
+      </div>
+      <p className="read-the-docs">Site is currently UNDER CONSTRUCTION</p>
     </>
   );
 }
